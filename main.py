82b000910831cc003cb93faed8b4769c2839faac
Lathiifa Zahira <lathiifa@Lathiifas-MacBook-Pro.local>
2026-05-05 14:47:31 +0700

    add starter backend and frontend

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from database import engine
+from models import Base
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def read_root():
+    return {"message": "Database connected!"}
