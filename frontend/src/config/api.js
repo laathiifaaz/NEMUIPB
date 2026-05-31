d4b51b29f782553c32e8099147a5e005f46a190b
booroobo <kazukor31@gmail.com>
2026-06-01 00:58:04 +0700

    Update API

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,2 +1,6 @@
-export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL.replace(/\/$/, "");
+const rawApiBaseUrl =
+  process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000";
+
+export const API_BASE_URL = rawApiBaseUrl
+  .replace(/^http:\/\/(.+\.up\.railway\.app)$/i, "https://$1")
+  .replace(/\/$/, "");
