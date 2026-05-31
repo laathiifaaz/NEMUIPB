a054f44d9581fe10e9ddf03f8818b90cd829f4da
booroobo <kazukor31@gmail.com>
2026-05-31 23:44:30 +0700

    Fix Origins and CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,11 +10,13 @@ app = FastAPI()
 
 origins = [
     "https://nemuipb.vercel.app",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
